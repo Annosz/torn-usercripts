afdb9e0c75f1fcdafd38a637d27758344c6b2e8e
Tóth Ádám <tadamleanyfalu@gmail.com>
2024-03-23 14:45:40 +0100

    Use waitForKeyElements instead of jQuery

--- larger_chain_timer.user.js
+++ larger_chain_timer.user.js
@@ -7,26 +7,24 @@
 // @match        https://www.torn.com/*
 // @updateURL    https://github.com/Annosz/torn-userscripts/raw/main/larger_chain_timer.user.js
 // @downloadURL  https://github.com/Annosz/torn-userscripts/raw/main/larger_chain_timer.user.js
-// @require      http://code.jquery.com/jquery-latest.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
+waitForKeyElements(".bar-timeleft___B9RGV", actionFunction);
+
+function actionFunction(jNode) {
     'use strict';
 
-    
-    $(document).ready(function()
-    {
-        const barStats = document.querySelector(".bar-stats___E_LqA");
-        const timeLeft = document.querySelector(".bar-timeleft___B9RGV");
-        const speed = document.querySelector(".speed___dFP2B");
-        const tickList = document.querySelector(".tick-list___McObN");
-    
-        barStats.style.display = "block";
-        timeLeft.style.fontSize = "60px";
-        timeLeft.style.height = "62px";
-        speed.style.top = "unset";
-        tickList.style.height = "8px";
-    })
-})();
+    const barStats = document.querySelector(".bar-stats___E_LqA");
+    const timeLeft = document.querySelector(".bar-timeleft___B9RGV");
+    const speed = document.querySelector(".speed___dFP2B");
+    const tickList = document.querySelector(".tick-list___McObN");
+
+    barStats.style.display = "block";
+    timeLeft.style.fontSize = "60px";
+    timeLeft.style.height = "62px";
+    speed.style.top = "unset";
+    tickList.style.height = "8px";
+};
