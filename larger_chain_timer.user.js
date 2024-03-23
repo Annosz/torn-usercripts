778347ecca30da72a2cf32d68801146c58648471
Tóth Ádám <tadamleanyfalu@gmail.com>
2024-03-23 14:05:50 +0100

    Only add larger chain timer on document load

--- larger_chain_timer.user.js
+++ larger_chain_timer.user.js
@@ -7,6 +7,7 @@
 // @match        https://www.torn.com/*
 // @updateURL    https://github.com/annosz/torn-userscripts/raw/master/larger_chain_timer.user.js
 // @downloadURL  https://github.com/annosz/torn-userscripts/raw/master/larger_chain_timer.user.js
+// @require      http://code.jquery.com/jquery-latest.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @grant        none
 // ==/UserScript==
@@ -14,14 +15,18 @@
 (function() {
     'use strict';
 
-    const barStats = document.querySelector(".bar-stats___E_LqA");
-    const timeLeft = document.querySelector(".bar-timeleft___B9RGV");
-    const speed = document.querySelector(".speed___dFP2B");
-    const tickList = document.querySelector(".tick-list___McObN");
-
-    barStats.style.display = "block";
-    timeLeft.style.fontSize = "60px";
-    timeLeft.style.height = "62px";
-    speed.style.top = "unset";
-    tickList.style.height = "8px";
+    
+    $(document).ready(function()
+    {
+        const barStats = document.querySelector(".bar-stats___E_LqA");
+        const timeLeft = document.querySelector(".bar-timeleft___B9RGV");
+        const speed = document.querySelector(".speed___dFP2B");
+        const tickList = document.querySelector(".tick-list___McObN");
+    
+        barStats.style.display = "block";
+        timeLeft.style.fontSize = "60px";
+        timeLeft.style.height = "62px";
+        speed.style.top = "unset";
+        tickList.style.height = "8px";
+    })
 })();
